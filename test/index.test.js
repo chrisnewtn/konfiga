2043a80602f02656e5fbc9b62f5db1f16ce77c7c
Chris Newton <chrisnewtn@gmail.com>
2017-11-26 13:47:40 +0000

    Convert project to two space indentation

--- test/index.test.js
+++ test/index.test.js
@@ -5,117 +5,117 @@ var sinon = require('sinon');
 var SandboxedModule = require('sandboxed-module');
 
 describe('konfiga', function() {
-    var konfiga;
-    var minimistStub;
-    var processConfigStub;
-    var fakeArgv;
-    var fakeEnv;
+  var konfiga;
+  var minimistStub;
+  var processConfigStub;
+  var fakeArgv;
+  var fakeEnv;
 
-    before(function() {
-        minimistStub = sinon.stub();
-        processConfigStub = sinon.stub();
+  before(function() {
+    minimistStub = sinon.stub();
+    processConfigStub = sinon.stub();
 
-        fakeArgv = ['a', 'command', '--an-arg', 'argValue'];
-        fakeEnv = {'AN_ARG': 'argValue'};
+    fakeArgv = ['a', 'command', '--an-arg', 'argValue'];
+    fakeEnv = {'AN_ARG': 'argValue'};
 
-        konfiga = SandboxedModule.require('../index', {
-            requires: {
-                minimist: minimistStub,
-                './lib/processConfig': processConfigStub,
-                './lib/defaultParsers': new Map([['some', 'parsers']])
-            },
-            globals: {
-                process: {
-                    argv: fakeArgv,
-                    env: fakeEnv
-                }
-            }
-        });
+    konfiga = SandboxedModule.require('../index', {
+      requires: {
+        minimist: minimistStub,
+        './lib/processConfig': processConfigStub,
+        './lib/defaultParsers': new Map([['some', 'parsers']])
+      },
+      globals: {
+        process: {
+          argv: fakeArgv,
+          env: fakeEnv
+        }
+      }
+    });
+  });
+
+  beforeEach(function() {
+    minimistStub.returns('fakeMinimistOutput');
+    processConfigStub.returns('fakeProcessedConfig');
+  });
+
+  afterEach(function() {
+    minimistStub.reset();
+    processConfigStub.reset();
+  });
+
+  it('is a function', function() {
+    assert.strictEqual(typeof konfiga, 'function');
+  });
+
+  it('has an arity of 2', function() {
+    assert.strictEqual(konfiga.length, 2);
+  });
+
+  it('returns a processed config object', function() {
+    assert.strictEqual(konfiga('fakeSchema'), 'fakeProcessedConfig');
+  });
+
+  it('passes the first argument (schema) to processConfig', function() {
+    konfiga('fakeSchema');
+
+    assert.strictEqual(processConfigStub.args[0][0], 'fakeSchema');
+  });
+
+  it('passes the argv option to processConfig if present', function() {
+    var customArgvObject = {some: 'rubbish'};
+
+    konfiga('fakeSchema', {argv: customArgvObject});
+
+    assert.strictEqual(processConfigStub.args[0][1], customArgvObject);
+  });
+
+  it('passes the env option to processConfig if present', function() {
+    var customEnvObject = {some: 'rubbish'};
+
+    konfiga('fakeSchema', {env: customEnvObject});
+
+    assert.strictEqual(processConfigStub.args[0][2], customEnvObject);
+  });
+
+  it('passes the parsers option merged with default parsers to processConfig if present', function() {
+    konfiga('fakeSchema', {parsers: [{type: 'more', parser: 'custom-parsers'}]});
+
+    assert.equal(processConfigStub.args[0][3].size, 2);
+    assert.equal(processConfigStub.args[0][3].get('some'), 'parsers');
+    assert.equal(processConfigStub.args[0][3].get('more'), 'custom-parsers');
+  });
+
+  describe('if no argv option is passed', function() {
+    it('passes every arg after the second of process.argv to minimist', function() {
+      konfiga('fakeSchema');
+
+      assert.strictEqual(minimistStub.callCount, 1);
+      assert.strictEqual(minimistStub.calledWith(['--an-arg', 'argValue']), true);
     });
 
-    beforeEach(function() {
-        minimistStub.returns('fakeMinimistOutput');
-        processConfigStub.returns('fakeProcessedConfig');
-    });
-
-    afterEach(function() {
-        minimistStub.reset();
-        processConfigStub.reset();
-    });
-
-    it('is a function', function() {
-        assert.strictEqual(typeof konfiga, 'function');
-    });
-
-    it('has an arity of 2', function() {
-        assert.strictEqual(konfiga.length, 2);
-    });
-
-    it('returns a processed config object', function() {
-        assert.strictEqual(konfiga('fakeSchema'), 'fakeProcessedConfig');
-    });
-
-    it('passes the first argument (schema) to processConfig', function() {
+    describe('the value returned by minimist', function() {
+      it('is passed to processConfig', function() {
         konfiga('fakeSchema');
 
-        assert.strictEqual(processConfigStub.args[0][0], 'fakeSchema');
+        assert.strictEqual(processConfigStub.args[0][1], 'fakeMinimistOutput');
+      });
     });
+  });
 
-    it('passes the argv option to processConfig if present', function() {
-        var customArgvObject = {some: 'rubbish'};
+  describe('if no env option is passed', function() {
+    it('passes proccess.env to processConfig', function() {
+      konfiga('fakeSchema');
 
-        konfiga('fakeSchema', {argv: customArgvObject});
-
-        assert.strictEqual(processConfigStub.args[0][1], customArgvObject);
+      assert.strictEqual(processConfigStub.args[0][2], fakeEnv);
     });
+  });
 
-    it('passes the env option to processConfig if present', function() {
-        var customEnvObject = {some: 'rubbish'};
+  describe('if no parsers option is passed', function() {
+    it('passes default parsers into the processConfig', function() {
+      konfiga('fakeSchema');
 
-        konfiga('fakeSchema', {env: customEnvObject});
-
-        assert.strictEqual(processConfigStub.args[0][2], customEnvObject);
+      assert.equal(processConfigStub.args[0][3].size, 1);
+      assert.equal(processConfigStub.args[0][3].get('some'), 'parsers');
     });
-
-    it('passes the parsers option merged with default parsers to processConfig if present', function() {
-        konfiga('fakeSchema', {parsers: [{type: 'more', parser: 'custom-parsers'}]});
-
-        assert.equal(processConfigStub.args[0][3].size, 2);
-        assert.equal(processConfigStub.args[0][3].get('some'), 'parsers');
-        assert.equal(processConfigStub.args[0][3].get('more'), 'custom-parsers');
-    });
-
-    describe('if no argv option is passed', function() {
-        it('passes every arg after the second of process.argv to minimist', function() {
-            konfiga('fakeSchema');
-
-            assert.strictEqual(minimistStub.callCount, 1);
-            assert.strictEqual(minimistStub.calledWith(['--an-arg', 'argValue']), true);
-        });
-
-        describe('the value returned by minimist', function() {
-            it('is passed to processConfig', function() {
-                konfiga('fakeSchema');
-
-                assert.strictEqual(processConfigStub.args[0][1], 'fakeMinimistOutput');
-            });
-        });
-    });
-
-    describe('if no env option is passed', function() {
-        it('passes proccess.env to processConfig', function() {
-            konfiga('fakeSchema');
-
-            assert.strictEqual(processConfigStub.args[0][2], fakeEnv);
-        });
-    });
-
-    describe('if no parsers option is passed', function() {
-        it('passes default parsers into the processConfig', function() {
-            konfiga('fakeSchema');
-
-            assert.equal(processConfigStub.args[0][3].size, 1);
-            assert.equal(processConfigStub.args[0][3].get('some'), 'parsers');
-        });
-    });
+  });
 });
--- test/lib/castValue.test.js
+++ test/lib/castValue.test.js
@@ -5,64 +5,64 @@ var sinon = require('sinon');
 var castValue = require('../../lib/castValue');
 
 describe('castValue', function() {
-    var parsers;
+  var parsers;
+
+  beforeEach(function() {
+    parsers = new Map([
+      ['test-type', sinon.stub().returns('returned-by-parser-two')],
+      ['default', sinon.stub().returns('returned-by-default-parser')]
+    ]);
+  });
+
+  it('is a function', function() {
+    assert.strictEqual(typeof castValue, 'function');
+  });
+
+  it('has an arity of 3', function() {
+    assert.strictEqual(castValue.length, 3);
+  });
+
+  it('throws if the type passed is not supported', function() {
+    function UnsupportedType() {}
+
+    function test() {
+      castValue(null, UnsupportedType, parsers);
+    }
+
+    assert.throws(test, 'Unsupported config value type: UnsupportedType');
+  });
+
+  describe('when passed a value with no type', function() {
+    var value;
+    var parsed;
 
     beforeEach(function() {
-        parsers = new Map([
-            ['test-type', sinon.stub().returns('returned-by-parser-two')],
-            ['default', sinon.stub().returns('returned-by-default-parser')]
-        ]);
+      value = {toString: sinon.stub().returns('gunk')};
+      parsed = castValue(value, undefined, parsers);
     });
 
-    it('is a function', function() {
-        assert.strictEqual(typeof castValue, 'function');
+    it('calls the default parser with the value', function() {
+      assert.deepStrictEqual(parsers.get('default').args, [[value]]);
     });
 
-    it('has an arity of 3', function() {
-        assert.strictEqual(castValue.length, 3);
+    it('returns the result of default parser', function() {
+      assert.strictEqual(parsed, 'returned-by-default-parser');
+    });
+  });
+
+  describe('when passed a valid type', function() {
+    var parsed;
+
+    beforeEach(function() {
+      parsed = castValue('blah', 'test-type', parsers);
     });
 
-    it('throws if the type passed is not supported', function() {
-        function UnsupportedType() {}
-
-        function test() {
-            castValue(null, UnsupportedType, parsers);
-        }
-
-        assert.throws(test, 'Unsupported config value type: UnsupportedType');
+    it('calls the appropriate parser with the given value', function() {
+      assert.ok(parsers.get('test-type').callCount, 1);
     });
 
-    describe('when passed a value with no type', function() {
-        var value;
-        var parsed;
-
-        beforeEach(function() {
-            value = {toString: sinon.stub().returns('gunk')};
-            parsed = castValue(value, undefined, parsers);
-        });
-
-        it('calls the default parser with the value', function() {
-            assert.deepStrictEqual(parsers.get('default').args, [[value]]);
-        });
-
-        it('returns the result of default parser', function() {
-            assert.strictEqual(parsed, 'returned-by-default-parser');
-        });
+    it('returns the retured value of the parser', function() {
+      assert.equal(parsed, 'returned-by-parser-two');
     });
-
-    describe('when passed a valid type', function() {
-        var parsed;
-
-        beforeEach(function() {
-            parsed = castValue('blah', 'test-type', parsers);
-        });
-
-        it('calls the appropriate parser with the given value', function() {
-            assert.ok(parsers.get('test-type').callCount, 1);
-        });
-
-        it('returns the retured value of the parser', function() {
-            assert.equal(parsed, 'returned-by-parser-two');
-        });
-    });
+  });
 });
