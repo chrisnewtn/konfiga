4825af16ce179bfea8b265f59ba8a48f7bd5b754
Chris Newton <chrisnewtn@gmail.com>
2017-11-26 13:47:40 +0000

    Convert project to two space indentation

--- test/lib/defaultParsers.test.js
+++ test/lib/defaultParsers.test.js
@@ -4,93 +4,93 @@ var assert = require('assert');
 var defaultParsers = require('../../lib/defaultParsers');
 
 describe('defaultParsers', function() {
-    describe('default', function() {
-        it('calls toString on the given value, and returns the result', function() {
-            var value = {
-                toString: function() {
-                    return 'to-string-result';
-                }
-            }
+  describe('default', function() {
+    it('calls toString on the given value, and returns the result', function() {
+      var value = {
+        toString: function() {
+          return 'to-string-result';
+        }
+      }
 
-            assert.equal(defaultParsers.get('default')(value), 'to-string-result');
-        });
+      assert.equal(defaultParsers.get('default')(value), 'to-string-result');
+    });
+  });
+
+  describe('String', function() {
+    it('calls toString on the given value, and returns the result', function() {
+      var value = {
+        toString: function() {
+          return 'to-string-result';
+        }
+      }
+
+      assert.equal(defaultParsers.get('default')(value), 'to-string-result');
+    });
+  });
+
+  describe('Boolean', function() {
+    it('returns true if the value is "true"', function() {
+      assert.strictEqual(defaultParsers.get(Boolean)('true'), true);
     });
 
-    describe('String', function() {
-        it('calls toString on the given value, and returns the result', function() {
-            var value = {
-                toString: function() {
-                    return 'to-string-result';
-                }
-            }
-
-            assert.equal(defaultParsers.get('default')(value), 'to-string-result');
-        });
+    it('returns true if the value is true', function() {
+      assert.strictEqual(defaultParsers.get(Boolean)(true), true);
     });
 
-    describe('Boolean', function() {
-        it('returns true if the value is "true"', function() {
-            assert.strictEqual(defaultParsers.get(Boolean)('true'), true);
-        });
+    it('returns false if the value is falsey', function() {
+      assert.strictEqual(defaultParsers.get(Boolean)(undefined), false);
+    });
+  });
 
-        it('returns true if the value is true', function() {
-            assert.strictEqual(defaultParsers.get(Boolean)(true), true);
-        });
-
-        it('returns false if the value is falsey', function() {
-            assert.strictEqual(defaultParsers.get(Boolean)(undefined), false);
-        });
+  describe('Number', function() {
+    it('returns a number', function() {
+      assert.strictEqual(defaultParsers.get(Number)(12.34), 12.34);
     });
 
-    describe('Number', function() {
-        it('returns a number', function() {
-            assert.strictEqual(defaultParsers.get(Number)(12.34), 12.34);
-        });
-
-        it('destringifies an integer', function() {
-            assert.strictEqual(defaultParsers.get(Number)('12'), 12);
-        });
-
-        it('destringifies a decimal', function() {
-            assert.strictEqual(defaultParsers.get(Number)('12.34'), 12.34);
-        });
-
-        it('destringifies other things to NaN', function() {
-            assert.ok(Number.isNaN(defaultParsers.get(Number)('blah')));
-        });
+    it('destringifies an integer', function() {
+      assert.strictEqual(defaultParsers.get(Number)('12'), 12);
     });
 
-    describe('Array', function() {
-        it('returns an empty array if the value is falsey', function() {
-            var returnedValue = defaultParsers.get(Array)(undefined);
-
-            assert.strictEqual(Array.isArray(returnedValue), true);
-            assert.strictEqual(returnedValue.length, 0);
-        });
-
-        it('returns the passed value if it is already an array', function() {
-            var passedValue = [1, 2, 3];
-            var returnedValue = defaultParsers.get(Array)(passedValue);
-
-            assert.strictEqual(returnedValue, passedValue);
-        });
-
-        it('returns the passed comma delimited string as an array', function() {
-            var returnedValue = defaultParsers.get(Array)('foo,bar,baz');
-
-            assert.strictEqual(Array.isArray(returnedValue), true);
-
-            assert.strictEqual(returnedValue[0], 'foo');
-            assert.strictEqual(returnedValue[1], 'bar');
-            assert.strictEqual(returnedValue[2], 'baz');
-        });
-
-        it('casts a number to a string before returning it wrapped in an array', function() {
-            var returnedValue = defaultParsers.get(Array)(123);
-
-            assert.strictEqual(Array.isArray(returnedValue), true);
-            assert.strictEqual(returnedValue.length, 1);
-            assert.strictEqual(returnedValue[0], '123');
-        });
+    it('destringifies a decimal', function() {
+      assert.strictEqual(defaultParsers.get(Number)('12.34'), 12.34);
     });
+
+    it('destringifies other things to NaN', function() {
+      assert.ok(Number.isNaN(defaultParsers.get(Number)('blah')));
+    });
+  });
+
+  describe('Array', function() {
+    it('returns an empty array if the value is falsey', function() {
+      var returnedValue = defaultParsers.get(Array)(undefined);
+
+      assert.strictEqual(Array.isArray(returnedValue), true);
+      assert.strictEqual(returnedValue.length, 0);
+    });
+
+    it('returns the passed value if it is already an array', function() {
+      var passedValue = [1, 2, 3];
+      var returnedValue = defaultParsers.get(Array)(passedValue);
+
+      assert.strictEqual(returnedValue, passedValue);
+    });
+
+    it('returns the passed comma delimited string as an array', function() {
+      var returnedValue = defaultParsers.get(Array)('foo,bar,baz');
+
+      assert.strictEqual(Array.isArray(returnedValue), true);
+
+      assert.strictEqual(returnedValue[0], 'foo');
+      assert.strictEqual(returnedValue[1], 'bar');
+      assert.strictEqual(returnedValue[2], 'baz');
+    });
+
+    it('casts a number to a string before returning it wrapped in an array', function() {
+      var returnedValue = defaultParsers.get(Array)(123);
+
+      assert.strictEqual(Array.isArray(returnedValue), true);
+      assert.strictEqual(returnedValue.length, 1);
+      assert.strictEqual(returnedValue[0], '123');
+    });
+  });
 });
--- test/lib/processConfig.test.js
+++ test/lib/processConfig.test.js
@@ -5,228 +5,228 @@ var sinon = require('sinon');
 var SandboxedModule = require('sandboxed-module');
 
 describe('processConfig', function() {
+  var exampleSchema;
+  var processConfig;
+  var castValueStub;
+
+  before(function() {
+    castValueStub = sinon.stub();
+
+    processConfig = SandboxedModule.require('../../lib/processConfig', {
+      requires: {
+        './castValue': castValueStub
+      }
+    });
+
+    exampleSchema = {
+      testOption: {
+        defaultValue: 'defaultValue',
+        envVariableName: 'TEST_OPTION',
+        cmdLineArgName: 'test-option',
+        type: String
+      },
+      anotherTestOption: {
+        defaultValue: 42,
+        envVariableName: 'ANOTHER_TEST_OPTION',
+        cmdLineArgName: 'another-test-option',
+        type: Number
+      }
+    };
+  });
+
+  afterEach(function() {
+    castValueStub.reset();
+  });
+
+  it('is a function', function() {
+    assert.strictEqual(typeof processConfig, 'function');
+  });
+
+  it('has an arity of 4', function() {
+    assert.strictEqual(processConfig.length, 4);
+  });
+
+  it('returns a frozen object', function() {
+    var returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
+
+    assert.strictEqual(Object.isFrozen(returnedObject), true);
+  });
+
+  describe('required options', function() {
     var exampleSchema;
-    var processConfig;
-    var castValueStub;
+    var argv;
+    var env;
 
     before(function() {
-        castValueStub = sinon.stub();
+      exampleSchema = {
+        testOption: {
+          required: true,
+          envVariableName: 'TEST_OPTION',
+          cmdLineArgName: 'test-option',
+          type: String
+        },
+        anotherTestOption: {
+          required: true,
+          envVariableName: 'ANOTHER_TEST_OPTION',
+          cmdLineArgName: 'another-test-option',
+          type: Number
+        }
+      };
 
-        processConfig = SandboxedModule.require('../../lib/processConfig', {
-            requires: {
-                './castValue': castValueStub
-            }
-        });
+      argv = {
+        'test-option': 'blah'
+      };
 
-        exampleSchema = {
-            testOption: {
-                defaultValue: 'defaultValue',
-                envVariableName: 'TEST_OPTION',
-                cmdLineArgName: 'test-option',
-                type: String
-            },
-            anotherTestOption: {
-                defaultValue: 42,
-                envVariableName: 'ANOTHER_TEST_OPTION',
-                cmdLineArgName: 'another-test-option',
-                type: Number
-            }
+      env = {
+        'TEST_OPTION': 'meh',
+        'ANOTHER_TEST_OPTION': '1234'
+      };
+
+      castValueStub.withArgs('blah', String).returns('fakeCastCliString');
+      castValueStub.withArgs('1234', Number).returns('fakeCastEnvNumber');
+    });
+
+    it('throws when required options are missing', function() {
+      assert.throws(
+        function() {
+          processConfig(exampleSchema, {}, {}, 'the-parsers');
+        },
+        Error,
+        'Missing required config for: testOption, anotherTestOption'
+      );
+    });
+
+    it('does not throw when no required options are missing', function() {
+      assert.doesNotThrow(function() {
+        processConfig(exampleSchema, argv, env, 'the-parsers');
+      });
+    });
+
+    it('gives precedence to CLI arguments', function() {
+      processConfig(exampleSchema, argv, env, 'the-parsers');
+
+      assert(castValueStub.calledWith('blah', String, 'the-parsers'));
+      assert(castValueStub.calledWith('1234', Number, 'the-parsers'));
+    });
+
+    it('puts parsed required args on the returned object', function() {
+      var config = processConfig(exampleSchema, argv, env, 'the-parsers');
+
+      assert.deepEqual(config, {
+        testOption: 'fakeCastCliString',
+        anotherTestOption: 'fakeCastEnvNumber'
+      });
+    });
+  });
+
+  describe('each option of the schema passed', function() {
+    var argv;
+
+    beforeEach(function(){
+      argv = {
+        'test-option': 'testCliValue',
+        'another-test-option': '52'
+      };
+
+      castValueStub.withArgs('testCliValue', String).returns('fakeCastCliString');
+      castValueStub.withArgs('52', Number).returns('fakeCastCliNumber');
+    });
+
+    it('is present on the returned object', function() {
+      var returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
+
+      assert(returnedObject.hasOwnProperty('testOption'));
+      assert(returnedObject.hasOwnProperty('anotherTestOption'));
+    });
+
+    describe('each command line value', function(){
+      var returnedObject;
+
+      beforeEach(function(){
+        returnedObject = processConfig(exampleSchema, argv, {}, 'the-parsers');
+      });
+
+      it('is cast to the type specified in the schema', function() {
+        assert.strictEqual(castValueStub.callCount, 2);
+
+        assert(castValueStub.calledWith('testCliValue', String, 'the-parsers'));
+        assert(castValueStub.calledWith('52', Number, 'the-parsers'));
+      });
+
+      it('has its cast value set on the return object', function() {
+        assert.strictEqual(returnedObject.testOption, 'fakeCastCliString');
+        assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastCliNumber');
+      });
+    });
+
+    describe('each environment variable value', function(){
+      var returnedObject;
+      var env;
+
+      beforeEach(function(){
+        env = {
+          'TEST_OPTION': 'testEnvValue',
+          'ANOTHER_TEST_OPTION': '48'
         };
+
+        castValueStub.withArgs('testEnvValue', String).returns('fakeCastEnvString');
+        castValueStub.withArgs('48', Number).returns('fakeCastEnvNumber');
+      });
+
+      describe('if no equivilant command line value is found', function() {
+        beforeEach(function() {
+          returnedObject = processConfig(exampleSchema, {}, env, 'the-parsers');
+        });
+
+        it('is cast to the type specified in the schema', function() {
+          assert.strictEqual(castValueStub.callCount, 2);
+
+          assert(castValueStub.calledWith('testEnvValue', String, 'the-parsers'));
+          assert(castValueStub.calledWith('48', Number, 'the-parsers'));
+        });
+
+        it('has its cast value set on the return object', function() {
+          assert.strictEqual(returnedObject.testOption, 'fakeCastEnvString');
+          assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastEnvNumber');
+        });
+      });
+
+      describe('if command line values are found also', function() {
+        beforeEach(function() {
+          returnedObject = processConfig(exampleSchema, argv, env, 'the-parsers');
+        });
+
+        it('casts and appends the command line values instead', function() {
+          assert.strictEqual(castValueStub.callCount, 2);
+
+          assert(castValueStub.calledWith('testCliValue', String, 'the-parsers'));
+          assert(castValueStub.calledWith('52', Number, 'the-parsers'));
+
+          assert.strictEqual(returnedObject.testOption, 'fakeCastCliString');
+          assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastCliNumber');
+        });
+      });
     });
 
-    afterEach(function() {
-        castValueStub.reset();
+    describe('if no command line or environment values are found', function() {
+      var returnedObject;
+
+      beforeEach(function() {
+        castValueStub.withArgs('defaultValue', String).returns('fakeCastDefaultString');
+        castValueStub.withArgs(42, Number).returns('fakeCastDefaultNumber');
+
+        returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
+      });
+
+      it('casts each default value onto the return object', function() {
+        assert.strictEqual(castValueStub.callCount, 2);
+
+        assert(castValueStub.calledWith('defaultValue', String, 'the-parsers'));
+        assert(castValueStub.calledWith(42, Number, 'the-parsers'));
+
+        assert.strictEqual(returnedObject.testOption, 'fakeCastDefaultString');
+        assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastDefaultNumber');
+      });
     });
-
-    it('is a function', function() {
-        assert.strictEqual(typeof processConfig, 'function');
-    });
-
-    it('has an arity of 4', function() {
-        assert.strictEqual(processConfig.length, 4);
-    });
-
-    it('returns a frozen object', function() {
-        var returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
-
-        assert.strictEqual(Object.isFrozen(returnedObject), true);
-    });
-
-    describe('required options', function() {
-        var exampleSchema;
-        var argv;
-        var env;
-
-        before(function() {
-            exampleSchema = {
-                testOption: {
-                    required: true,
-                    envVariableName: 'TEST_OPTION',
-                    cmdLineArgName: 'test-option',
-                    type: String
-                },
-                anotherTestOption: {
-                    required: true,
-                    envVariableName: 'ANOTHER_TEST_OPTION',
-                    cmdLineArgName: 'another-test-option',
-                    type: Number
-                }
-            };
-
-            argv = {
-                'test-option': 'blah'
-            };
-
-            env = {
-                'TEST_OPTION': 'meh',
-                'ANOTHER_TEST_OPTION': '1234'
-            };
-
-            castValueStub.withArgs('blah', String).returns('fakeCastCliString');
-            castValueStub.withArgs('1234', Number).returns('fakeCastEnvNumber');
-        });
-
-        it('throws when required options are missing', function() {
-            assert.throws(
-                function() {
-                    processConfig(exampleSchema, {}, {}, 'the-parsers');
-                },
-                Error,
-                'Missing required config for: testOption, anotherTestOption'
-            );
-        });
-
-        it('does not throw when no required options are missing', function() {
-            assert.doesNotThrow(function() {
-                processConfig(exampleSchema, argv, env, 'the-parsers');
-            });
-        });
-
-        it('gives precedence to CLI arguments', function() {
-            processConfig(exampleSchema, argv, env, 'the-parsers');
-
-            assert(castValueStub.calledWith('blah', String, 'the-parsers'));
-            assert(castValueStub.calledWith('1234', Number, 'the-parsers'));
-        });
-
-        it('puts parsed required args on the returned object', function() {
-            var config = processConfig(exampleSchema, argv, env, 'the-parsers');
-
-            assert.deepEqual(config, {
-                testOption: 'fakeCastCliString',
-                anotherTestOption: 'fakeCastEnvNumber'
-            });
-        });
-    });
-
-    describe('each option of the schema passed', function() {
-        var argv;
-
-        beforeEach(function(){
-            argv = {
-                'test-option': 'testCliValue',
-                'another-test-option': '52'
-            };
-
-            castValueStub.withArgs('testCliValue', String).returns('fakeCastCliString');
-            castValueStub.withArgs('52', Number).returns('fakeCastCliNumber');
-        });
-
-        it('is present on the returned object', function() {
-            var returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
-
-            assert(returnedObject.hasOwnProperty('testOption'));
-            assert(returnedObject.hasOwnProperty('anotherTestOption'));
-        });
-
-        describe('each command line value', function(){
-            var returnedObject;
-
-            beforeEach(function(){
-                returnedObject = processConfig(exampleSchema, argv, {}, 'the-parsers');
-            });
-
-            it('is cast to the type specified in the schema', function() {
-                assert.strictEqual(castValueStub.callCount, 2);
-
-                assert(castValueStub.calledWith('testCliValue', String, 'the-parsers'));
-                assert(castValueStub.calledWith('52', Number, 'the-parsers'));
-            });
-
-            it('has its cast value set on the return object', function() {
-                assert.strictEqual(returnedObject.testOption, 'fakeCastCliString');
-                assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastCliNumber');
-            });
-        });
-
-        describe('each environment variable value', function(){
-            var returnedObject;
-            var env;
-
-            beforeEach(function(){
-                env = {
-                    'TEST_OPTION': 'testEnvValue',
-                    'ANOTHER_TEST_OPTION': '48'
-                };
-
-                castValueStub.withArgs('testEnvValue', String).returns('fakeCastEnvString');
-                castValueStub.withArgs('48', Number).returns('fakeCastEnvNumber');
-            });
-
-            describe('if no equivilant command line value is found', function() {
-                beforeEach(function() {
-                    returnedObject = processConfig(exampleSchema, {}, env, 'the-parsers');
-                });
-
-                it('is cast to the type specified in the schema', function() {
-                    assert.strictEqual(castValueStub.callCount, 2);
-
-                    assert(castValueStub.calledWith('testEnvValue', String, 'the-parsers'));
-                    assert(castValueStub.calledWith('48', Number, 'the-parsers'));
-                });
-
-                it('has its cast value set on the return object', function() {
-                    assert.strictEqual(returnedObject.testOption, 'fakeCastEnvString');
-                    assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastEnvNumber');
-                });
-            });
-
-            describe('if command line values are found also', function() {
-                beforeEach(function() {
-                    returnedObject = processConfig(exampleSchema, argv, env, 'the-parsers');
-                });
-
-                it('casts and appends the command line values instead', function() {
-                    assert.strictEqual(castValueStub.callCount, 2);
-
-                    assert(castValueStub.calledWith('testCliValue', String, 'the-parsers'));
-                    assert(castValueStub.calledWith('52', Number, 'the-parsers'));
-
-                    assert.strictEqual(returnedObject.testOption, 'fakeCastCliString');
-                    assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastCliNumber');
-                });
-            });
-        });
-
-        describe('if no command line or environment values are found', function() {
-            var returnedObject;
-
-            beforeEach(function() {
-                castValueStub.withArgs('defaultValue', String).returns('fakeCastDefaultString');
-                castValueStub.withArgs(42, Number).returns('fakeCastDefaultNumber');
-
-                returnedObject = processConfig(exampleSchema, {}, {}, 'the-parsers');
-            });
-
-            it('casts each default value onto the return object', function() {
-                assert.strictEqual(castValueStub.callCount, 2);
-
-                assert(castValueStub.calledWith('defaultValue', String, 'the-parsers'));
-                assert(castValueStub.calledWith(42, Number, 'the-parsers'));
-
-                assert.strictEqual(returnedObject.testOption, 'fakeCastDefaultString');
-                assert.strictEqual(returnedObject.anotherTestOption, 'fakeCastDefaultNumber');
-            });
-        });
-    });
+  });
 });
